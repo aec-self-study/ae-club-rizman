6de6e0f32fd149c60046d3449fa71ccbb92ca899
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-06 02:40:59 +0000

    changed alias

--- customers.sql
+++ customers.sql
@@ -1,14 +1,14 @@
 SELECT
 Customers.id
 , Customers.Name
-, COUNT(DISTINCT(Orders.Id)) AS Total_Orders
-, MIN(Orders.Created_At) AS First_Order_Timestamp
-, MAX(Orders.Created_At) AS Last_Order_Timestamp
+, COUNT(DISTINCT(Cust_Orders.Id)) AS Total_Orders
+, MIN(Cust_Orders.Created_At) AS First_Order_Timestamp
+, MAX(Cust_Orders.Created_At) AS Last_Order_Timestamp
  
  FROM
  analytics-engineers-club.coffee_shop.customers customers
- LEFT JOIN analytics-engineers-club.coffee_shop.orders Orders
- ON Customers.id = Orders.customer_id
+ LEFT JOIN analytics-engineers-club.coffee_shop.orders Cust_Orders
+ ON Customers.id = Cust_Orders.customer_id
 
  GROUP BY 1,2
  ORDER BY 4
