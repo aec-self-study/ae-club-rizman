f7a7dd5ae3f76ca9ac5900b9ea7f1688a8e46258
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-06 04:19:56 +0000

    fixed

--- coffee_shop_rizman/models/monthly_customers.sql
+++ coffee_shop_rizman/models/monthly_customers.sql
@@ -2,7 +2,7 @@
  
  select
   date_trunc(first_order_timestamp, month) AS month
-  , count(distinct(customer_id) as monthly_customers
+  , count(distinct(id)) as monthly_customers
 
-  from {{ref (customers)}}
+  from {{ref ('customers')}}
   group by 1
