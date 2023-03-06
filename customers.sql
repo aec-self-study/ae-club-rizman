ba79bead306f19ae6790da929ce0aa4faa80becb
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-06 01:25:50 +0000

    added null value

--- customers.sql
+++ customers.sql
@@ -3,7 +3,8 @@ Customers.id
 , Customers.Name
 , COUNT(DISTINCT(Orders.Id)) AS Total_Orders
 , MIN(Orders.Created_At) AS First_Order_Timestamp
- 
+, NULL AS null_field
+
  FROM
  analytics-engineers-club.coffee_shop.customers Customers
  LEFT JOIN analytics-engineers-club.coffee_shop.orders Orders
