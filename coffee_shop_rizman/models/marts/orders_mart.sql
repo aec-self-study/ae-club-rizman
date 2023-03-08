60d8580be668178b9ee6fde86c989eed62218a1f
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-08 02:54:45 +0000

    updated orders_mart sql

--- coffee_shop_rizman/models/marts/orders_mart.sql
+++ coffee_shop_rizman/models/marts/orders_mart.sql
@@ -6,16 +6,26 @@ select *
 from {{ ref ('orders_stg')}}
 ),
 
-staged as (
-select
-orders_stg.*
+customer_order_number as (
+select 
+orders_stg.order_id
 , count(*) over (partition by orders_stg.customer_id order by orders_stg.order_created_at) as customer_order_number
-, case
-    when count(*) over (partition by orders_stg.customer_id order by orders_stg.order_created_at) = 1 THEN 'new customer order'
-    else 'returning customer order'
-    end as new_or_returning_customer_order
 
 from orders_stg
+),
+
+orders as (
+select
+orders_stg.*
+, customer_order_number.customer_order_number
+, case 
+     when customer_order_number.customer_order_number = 1 THEN 'new customer order'
+     when customer_order_number.customer_order_number > 1 THEN 'returning customer order'
+     end as new_or_returning_customer_order
+
+from orders_stg
+left join customer_order_number on orders_stg.order_id = customer_order_number.order_id
 )
 
-select * from staged
+select * 
+from orders
