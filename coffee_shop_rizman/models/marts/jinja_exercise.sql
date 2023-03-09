dc972c20fe9ddd573b4e57b76bd79234f7900ca5
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-09 21:53:42 +0000

    rewrote query to include jinja

---
+++ coffee_shop_rizman/models/marts/jinja_exercise.sql
@@ -0,0 +1,17 @@
+{{config (materialized = 'table')}}
+{% set product_categories = ['coffee beans', 'merch',
+ 'brewing supplies'] %}
+
+select
+cast(date_trunc(order_created_at, month) as date) as date_month
+
+{% for product_category in product_categories %}
+, sum(case when product_category = '{{product_category}}' then product_price end) 
+  as {{product_category | replace (' ' , '_') }}_amount
+{% endfor %}
+
+, sum(product_price) as total_amount
+
+from {{ ref ('order_items_mart')}}
+group by 1
+order by 1
