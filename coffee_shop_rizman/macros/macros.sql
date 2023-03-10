044510da131e37829b0975b44d27d5376f3e6103
mrizman <70602050+mrizman@users.noreply.github.com>
2023-03-10 02:01:52 +0000

    added test that prices were not 0 or null and fixed issue with code for pageviews mart

--- coffee_shop_rizman/macros/macros.sql
+++ coffee_shop_rizman/macros/macros.sql
@@ -1,3 +1,12 @@
 {% macro product_categories_list() %}
 {{ return (['coffee beans', 'merch', 'brewing supplies'])}}
 {% endmacro %}
+
+
+{% test is_not_free(model, column_name) %}
+SELECT 
+{{ column_name }}
+FROM {{model}}
+where {{column_name}} is null
+or {{column_name}} < 0
+{% endtest %}
